7a474a4abca874bfdbe29da5130798bb7d768e8a
Wijayaac <wijayaac25@gmail.com>
2022-05-14 20:35:38 +0800

    Fix : update custom blocks source

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '994fd4d5647c4717dbc8e3da13d9c986');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '7a38b02a164a5a6be0ce1a10f72bc95f');
